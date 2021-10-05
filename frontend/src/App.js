bf2fcaf30c1fbf2e4dad6d59250a636ef850f0fd
Denis Romanov <dromanov@solit-clouds.ru>
2021-10-05 21:54:15 +0300

    Lesson #7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import AuthorList from './components/Authors.js';
 import BookList from './components/Books.js';
 import AuthorBookList from './components/AuthorBooks.js';
+import LoginForm from './components/LoginForm.js';
 
 const NotFound = ({location}) => {
     return (<div>Page not found: {location.pathname}</div>)
@@ -14,27 +15,71 @@ class App extends React.Component {
         super(props)
         this.state = {
             'authors': [],
-            'books': []
+            'books': [],
+            'token': ''
         }
     }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/authors/')
+    getToken(login, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/', {"username": login, "password": password})
+        .then(response => {
+            console.log(response.data.token)
+            localStorage.setItem('token', response.data.token)
+            this.setState({'token': response.data.token}, this.loadData)
+        })
+        .catch(error => alert("Wrong password"))
+    }
+
+    logout() {
+        localStorage.setItem('token', '')
+        this.setState({'token': ''}, this.loadData)
+    }
+
+    isAuthenticated() {
+        return !!this.state.token
+    }
+
+    getHeaders() {
+        if (this.isAuthenticated()) {
+            return {'Authorization': 'Token ' + this.state.token}
+        }
+        return {}
+    }
+
+    loadData() {
+        const headers = this.getHeaders()
+        axios.get('http://127.0.0.1:8000/api/authors/', {headers})
         .then(response => {
             const authors = response.data
             this.setState( {
                 'authors': authors
             })
         })
-        .catch(error => console.log(error))
-        axios.get('http://127.0.0.1:8000/api/books/')
+        .catch(error => {
+            console.log(error)
+            this.setState({
+                'authors': []
+            })
+        })
+        axios.get('http://127.0.0.1:8000/api/books/', {headers})
         .then(response => {
             const books = response.data
             this.setState( {
                 'books': books
             })
         })
-        .catch(error => console.log(error))
+        .catch(error => {
+            console.log(error)
+            this.setState({
+                'books': []
+            })
+        })
+    }
+
+    componentDidMount() {
+        const token = localStorage.getItem('token')
+        console.log(token)
+        this.setState({'token': token}, this.loadData)
     }
 
     render() {
@@ -45,12 +90,19 @@ class App extends React.Component {
                         <ul>
                             <li><Link to='/'>Authors</Link></li>
                             <li><Link to='/books'>Books</Link></li>
+                            <li>
+                                { this.isAuthenticated() ?
+                                    <button onClick={()=>this.logout()}>Logout</button> :
+                                    <Link to='/login'>Login</Link>
+                                }
+                            </li>
                         </ul>
                     </nav>
 
                     <Switch>
                         <Route path='/' exact component={() => <AuthorList authors = {this.state.authors}/>} />
                         <Route path='/books' exact component={() => <BookList books = {this.state.books}/>} />
+                        <Route path='/login' exact component={() => <LoginForm getToken={(login, password) => this.getToken(login, password)} />} />
                         <Route path='/author/:id' component={() => <AuthorBookList books = {this.state.books}/>} />
                         <Redirect from='/authors' to='/' />
                         <Route component={NotFound} />
